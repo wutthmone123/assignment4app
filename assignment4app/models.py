9ba5b0da41cf8ca95df19a263206fa7cf9ce1913
wutt hmone <hsuekhitlin2004@gmail.com>
2025-08-03 15:00:07 +0630

    first commit

--- assignment4app/models.py
+++ assignment4app/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class assignment4app(models.Model):
     GENDER_CHOICES = [
         ('', 'Choose gender'),
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
     ]
 
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=20,choices=GENDER_CHOICES)
     phone = models.CharField(max_length=15)
 
     def __str__(self):
